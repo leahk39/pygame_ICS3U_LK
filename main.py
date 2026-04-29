38f843ce3bbd9178b4c344b81ad2907da0c86a8d
leahk39 <leahk39@kprschools.ca>
2026-04-29 14:23:23 -0400

    video2

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ pygame.init()
 
 # create the screen
 screen = pygame.display.set_mode((800,600))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
