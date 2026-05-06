b03e253409fc2747872639c7d0326587396d9a4e
leahk39 <leahk39@kprschools.ca>
2026-05-06 11:59:27 -0400

    vid4

--- main.py
+++ main.py
@@ -11,12 +11,24 @@ pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('ufo1.png')
 pygame.display.set_icon(icon)
 
+# player
+playerImg = pygame.image.load('spaceship 1.png')
+playerX = 370
+playerY = 480
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
 # game loop
 running = True
 while running:
+
+    # RGB - red, green, blue
+    screen.fill((0,0,0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # RBG - red, green, blue
-    screen.fill((0,0,0))
+    player()
+    pygame.display.update()
